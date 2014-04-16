d21d412988cbeed549f42ad5c39004b672e884bc
Amos Haviv <mail@amoshaviv.com>
2014-04-16 12:55:47 +0300

    Fixed hashPassword

--- app/templates/app/models/user.js
+++ app/templates/app/models/user.js
@@ -93,7 +93,7 @@ UserSchema.pre('save', function(next) {
  * Create instance method for hashing a password
  */
 UserSchema.methods.hashPassword = function(password) {
-	if (password) {
+	if (this.salt && password) {
 		return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
 	} else {
 		return password;
