efeb974889b5ea681a5996062c532c6d4758463a
Keshav OS <keshav.os@gmail.com>
2014-07-16 17:48:15 +0100

    Add test for app creation with and without sample article application

--- test/test-creation.js
+++ test/test-creation.js
@@ -8,19 +8,18 @@ var assert = require('assert');
 var exec = require('child_process').exec;
 var async = require('async');
 
-describe('meanjs generator', function() {
-    this.timeout(15000);
+describe('meanjs generator without sample app', function() {
 
     /**
-    * Cleanup temp directory after tests
-    */
+     * Cleanup temp folder after test run
+     */
     after(function(){
         temp.cleanup();
     });
-    
-    // Mock user input as default options 
+
+
     beforeEach(function(done){
-        runGenerator('app', 
+        runGenerator('app',
             '',
             this, {
                 'appName' : 'MEAN',
@@ -31,81 +30,262 @@ describe('meanjs generator', function() {
             }, done
         );
     });
-    
 
-    it('creates expected files when sample article example is chosen', 
+    /**
+     * Should create expected files when sample article example is not chosen
+     */
+    it('should create expected files when sample article example is not chosen',
         function() {
 
         var expected = [
-            "app/controllers/core.js",
-            "app/controllers/users.js",
-            "app/models/user.js",
-            "app/routes/core.js",
-            "app/routes/users.js",
-            "app/tests/users.js",
-            "public/css/common.css",
-            "public/img/brand/favicon.ico",
-            "public/img/brand/logo.png",
-            "public/img/loaders/loader.gif",
-            "public/js/application.js",
-            "public/modules/users/config/config.js",
-            "public/modules/users/config/routes.js",
-            "public/modules/users/controllers/authentication.js",
-            "public/modules/users/controllers/settings.js",
+
+            "app/views/404.server.view.html",
+            "app/views/500.server.view.html",
+            "app/views/index.server.view.html",
+            "app/views/layout.server.view.html",
+            "app/controllers/core.server.controller.js",
+            "app/controllers/users.server.controller.js",
+            "app/models/user.server.model.js",
+            "app/routes/core.server.routes.js",
+            "app/routes/users.server.routes.js",
+            "app/tests/user.server.model.test.js",
+            "public/application.js",
+            "public/humans.txt",
+            "public/robots.txt",
+            "public/modules/users/config/users.client.config.js",
+            "public/modules/users/config/users.client.routes.js",
+            "public/modules/users/controllers/authentication.client.controller.js",
+            "public/modules/users/controllers/settings.client.controller.js",
             "public/modules/users/css/users.css",
             "public/modules/users/img/buttons/facebook.png",
             "public/modules/users/img/buttons/google.png",
             "public/modules/users/img/buttons/linkedin.png",
             "public/modules/users/img/buttons/twitter.png",
-            "public/modules/users/services/authentication.js",
-            "public/modules/users/services/users.js",
-            "public/modules/users/users.js",
-            "public/modules/users/views/settings/password.html",
-            "public/modules/users/views/settings/profile.html",
-            "public/modules/users/views/signin.html",
-            "public/modules/users/views/signup.html",
-            "public/modules/core/config/routes.js",
-            "public/modules/core/tests/header.spec.js",
-            "public/modules/core/tests/home.spec.js",
-            "public/modules/core/controllers/home.js",
-            "public/modules/core/views/home.html",
-            "public/modules/core/core.js",
+            "public/modules/users/services/authentication.client.service.js",
+            "public/modules/users/services/users.client.service.js",
+            "public/modules/users/tests/authentication.client.controller.test.js",
+            "public/modules/users/users.client.module.js",
+            "public/modules/users/views/settings/change-password.client.view.html",
+            "public/modules/users/views/settings/edit-profile.client.view.html",
+            "public/modules/users/views/settings/social-accounts.client.view.html",
+            "public/modules/users/views/signin.client.view.html",
+            "public/modules/users/views/signup.client.view.html",
+            "public/modules/core/config/core.client.routes.js",
+            "public/modules/core/controllers/header.client.controller.js",
+            "public/modules/core/controllers/home.client.controller.js",
+            "public/modules/core/css/core.css",
+            "public/modules/core/img/brand/favicon.ico",
+            "public/modules/core/img/brand/logo.png",
+            "public/modules/core/img/loaders/loader.gif",
+            "public/modules/core/services/menus.client.service.js",
+            "public/modules/core/tests/header.client.controller.test.js",
+            "public/modules/core/tests/home.client.controller.test.js",
+            "public/modules/core/views/home.client.view.html",
+            "public/modules/core/core.client.module.js",
             "config/strategies/facebook.js",
             "config/strategies/google.js",
             "config/strategies/linkedin.js",
             "config/strategies/local.js",
             "config/strategies/twitter.js",
             "config/config.js",
+            "config/init.js",
             "config/express.js",
             "config/passport.js",
-            "config/utilities.js",
+            "karma.conf.js",
             "gruntfile.js",
             "server.js",
             "Procfile",
             "README.md",
+            "LICENSE.md",
             ".bowerrc",
+            ".csslintrc",
             ".jshintrc",
             ".gitignore",
             ".slugignore",
             ".travis.yml",
-            "app/views/404.html",
-            "app/views/500.html",
-            "app/views/index.html",
-            "app/views/layout.html",
             "config/env/all.js",
             "config/env/development.js",
             "config/env/production.js",
             "config/env/test.js",
-            "config/env/travis.js",
-            "public/js/config.js",
-            "public/modules/core/views/header.html",
-            "public/modules/core/controllers/header.js",
+            "public/config.js",
+            "public/modules/core/views/header.client.view.html",
             "package.json",
-            "bower.json",
-            "karma.conf.js"
+            "bower.json"
         ];
 
-        helpers.assertFile(expected);
+        assert.file(expected);
+    });
+
+    /**
+     * Should create expected files when sample article is chosen
+     */
+    it('should create expected files when sample article is chosen', function(){
+        var expected =[
+            "app/views/404.server.view.html",
+            "app/views/500.server.view.html",
+            "app/views/index.server.view.html",
+            "app/views/layout.server.view.html",
+            "app/controllers/core.server.controller.js",
+            "app/controllers/users.server.controller.js",
+            "app/models/user.server.model.js",
+            "app/routes/core.server.routes.js",
+            "app/routes/users.server.routes.js",
+            "app/tests/user.server.model.test.js",
+            "public/application.js",
+            "public/humans.txt",
+            "public/robots.txt",
+            "public/modules/users/config/users.client.config.js",
+            "public/modules/users/config/users.client.routes.js",
+            "public/modules/users/controllers/authentication.client.controller.js",
+            "public/modules/users/controllers/settings.client.controller.js",
+            "public/modules/users/css/users.css",
+            "public/modules/users/img/buttons/facebook.png",
+            "public/modules/users/img/buttons/google.png",
+            "public/modules/users/img/buttons/linkedin.png",
+            "public/modules/users/img/buttons/twitter.png",
+            "public/modules/users/services/authentication.client.service.js",
+            "public/modules/users/services/users.client.service.js",
+            "public/modules/users/tests/authentication.client.controller.test.js",
+            "public/modules/users/users.client.module.js",
+            "public/modules/users/views/settings/change-password.client.view.html",
+            "public/modules/users/views/settings/edit-profile.client.view.html",
+            "public/modules/users/views/settings/social-accounts.client.view.html",
+            "public/modules/users/views/signin.client.view.html",
+            "public/modules/users/views/signup.client.view.html",
+            "public/modules/core/config/core.client.routes.js",
+            "public/modules/core/controllers/header.client.controller.js",
+            "public/modules/core/controllers/home.client.controller.js",
+            "public/modules/core/css/core.css",
+            "public/modules/core/img/brand/favicon.ico",
+            "public/modules/core/img/brand/logo.png",
+            "public/modules/core/img/loaders/loader.gif",
+            "public/modules/core/services/menus.client.service.js",
+            "public/modules/core/tests/header.client.controller.test.js",
+            "public/modules/core/tests/home.client.controller.test.js",
+            "public/modules/core/views/home.client.view.html",
+            "public/modules/core/core.client.module.js",
+            "config/strategies/facebook.js",
+            "config/strategies/google.js",
+            "config/strategies/linkedin.js",
+            "config/strategies/local.js",
+            "config/strategies/twitter.js",
+            "config/config.js",
+            "config/init.js",
+            "config/express.js",
+            "config/passport.js",
+            "karma.conf.js",
+            "gruntfile.js",
+            "server.js",
+            "Procfile",
+            "README.md",
+            "LICENSE.md",
+            ".bowerrc",
+            ".csslintrc",
+            ".jshintrc",
+            ".gitignore",
+            ".slugignore",
+            ".travis.yml"
+        ];
+
+        assert.file(expected);
+    });
+});
+
+describe('meanjs generator with sample app', function() {
+
+    /**
+     * Cleanup temp folder after test run
+     */
+    after(function(){
+        temp.cleanup();
+    });
+
+
+    beforeEach(function(done){
+        runGenerator('app',
+            '',
+            this, {
+                'appName' : 'MEAN',
+                'appDescription' : 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+                'appKeywords' : 'MongoDB, Express, AngularJS, Node.js',
+                'appAuthor' : 'Test',
+                'addArticleExample' : true
+            }, done
+        );
+    });
+
+    /*
+     * Should create expected files when sample article is chosen
+     */
+    it('should create expected files when sample article is chosen', function(){
+        var expected =[
+            "app/views/404.server.view.html",
+            "app/views/500.server.view.html",
+            "app/views/index.server.view.html",
+            "app/views/layout.server.view.html",
+            "app/controllers/core.server.controller.js",
+            "app/controllers/users.server.controller.js",
+            "app/models/user.server.model.js",
+            "app/routes/core.server.routes.js",
+            "app/routes/users.server.routes.js",
+            "app/tests/user.server.model.test.js",
+            "public/application.js",
+            "public/humans.txt",
+            "public/robots.txt",
+            "public/modules/users/config/users.client.config.js",
+            "public/modules/users/config/users.client.routes.js",
+            "public/modules/users/controllers/authentication.client.controller.js",
+            "public/modules/users/controllers/settings.client.controller.js",
+            "public/modules/users/css/users.css",
+            "public/modules/users/img/buttons/facebook.png",
+            "public/modules/users/img/buttons/google.png",
+            "public/modules/users/img/buttons/linkedin.png",
+            "public/modules/users/img/buttons/twitter.png",
+            "public/modules/users/services/authentication.client.service.js",
+            "public/modules/users/services/users.client.service.js",
+            "public/modules/users/tests/authentication.client.controller.test.js",
+            "public/modules/users/users.client.module.js",
+            "public/modules/users/views/settings/change-password.client.view.html",
+            "public/modules/users/views/settings/edit-profile.client.view.html",
+            "public/modules/users/views/settings/social-accounts.client.view.html",
+            "public/modules/users/views/signin.client.view.html",
+            "public/modules/users/views/signup.client.view.html",
+            "public/modules/core/config/core.client.routes.js",
+            "public/modules/core/controllers/header.client.controller.js",
+            "public/modules/core/controllers/home.client.controller.js",
+            "public/modules/core/css/core.css",
+            "public/modules/core/img/brand/favicon.ico",
+            "public/modules/core/img/brand/logo.png",
+            "public/modules/core/img/loaders/loader.gif",
+            "public/modules/core/services/menus.client.service.js",
+            "public/modules/core/tests/header.client.controller.test.js",
+            "public/modules/core/tests/home.client.controller.test.js",
+            "public/modules/core/views/home.client.view.html",
+            "public/modules/core/core.client.module.js",
+            "config/strategies/facebook.js",
+            "config/strategies/google.js",
+            "config/strategies/linkedin.js",
+            "config/strategies/local.js",
+            "config/strategies/twitter.js",
+            "config/config.js",
+            "config/init.js",
+            "config/express.js",
+            "config/passport.js",
+            "karma.conf.js",
+            "gruntfile.js",
+            "server.js",
+            "Procfile",
+            "README.md",
+            "LICENSE.md",
+            ".bowerrc",
+            ".csslintrc",
+            ".jshintrc",
+            ".gitignore",
+            ".slugignore",
+            ".travis.yml"
+        ];
+
+        assert.file(expected);
     });
 });
 
