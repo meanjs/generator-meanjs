3e7c5997cfac41c766e3d29f022a625a6840819b
jloveland <jason.c.loveland@gmail.com>
2014-10-27 21:54:11 -0400

    removed minified js lib files due to issue #183

--- app/templates/config/env/_secure.js
+++ app/templates/config/env/_secure.js
@@ -4,27 +4,6 @@ module.exports = {
   secure: true,
   port: 443,
   db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/<%= slugifiedAppName %>',
-  assets: {
-  lib: {
-    css: [
-      'public/lib/bootstrap/dist/css/bootstrap.min.css',
-      'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-    ],
-    js: [
-      'public/lib/angular/angular.min.js',
-      'public/lib/angular-resource/angular-resource.js', <% if (angularCookies) { %>
-      'public/lib/angular-cookies/angular-cookies.js', <% } if (angularAnimate) { %>
-      'public/lib/angular-animate/angular-animate.js', <% } if (angularTouch) { %>
-      'public/lib/angular-touch/angular-touch.js', <% } if (angularSanitize) { %>
-      'public/lib/angular-sanitize/angular-sanitize.js', <% } %>
-      'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-      'public/lib/angular-ui-utils/ui-utils.min.js',
-      'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
-    ]
-  },
-  css: 'public/dist/application.min.css',
-  js: 'public/dist/application.min.js'
-  },
   facebook: {
     clientID: process.env.FACEBOOK_ID || 'APP_ID',
     clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
