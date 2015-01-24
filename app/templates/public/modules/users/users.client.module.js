226a8b2977be5b59754f1fd9adcfe6e0cdeb0bea
Apmats <amatsagkas@gmail.com>
2015-01-24 15:09:54 +0200

    fixed a spelling mistake

--- app/templates/public/modules/users/users.client.module.js
+++ app/templates/public/modules/users/users.client.module.js
@@ -1,4 +1,4 @@
 'use strict';
 
-// Use Applicaion configuration module to register a new module
+// Use application configuration module to register a new module
 ApplicationConfiguration.registerModule('users');
