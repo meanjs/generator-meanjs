015efac0891c9c687a0701450a001d60cfa24bb2
Cody B. Daig <cody@daig.me>
2015-09-28 09:43:29 -0700

    [feat] Remove Templates

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,5 @@
 var path = require('path'),
+  fs = require('fs'),
   helpers = require('yeoman-generator').test,
   assert = require('yeoman-generator').assert,
   temp = require('temp').track();
@@ -8,19 +9,19 @@ describe('Main Generator', function () {
   /**
    * Setup the temp directory
    */
-  beforeEach(function (done) {
+  before(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), done);
   });
 
   /**
    * Clean up temp directory
    */
-  afterEach(function () {
+  after(function () {
     temp.cleanup();
   });
 
   describe('Application generator without sample module', function () {
-    beforeEach(function (done) {
+    before(function (done) {
       helpers.run(path.join(__dirname, '../app'))
         .withOptions({
           'skip-install': true
@@ -29,8 +30,8 @@ describe('Main Generator', function () {
         .withPrompts({
           version: '0.4.0',
           folder: 'temp',
-          appName: 'MEAN',
-          appDescription: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+          appName: 'MEANtestAppName',
+          appDescription: 'Testing the generator',
           appKeywords: 'MongoDB, Express, AngularJS, Node.js',
           appAuthor: 'Test',
           addArticleExample: false,
@@ -39,11 +40,27 @@ describe('Main Generator', function () {
         .on('ready', function (generator) {
           // this is called right before `generator.run()` is called
         })
-        .on('end', done);
+        .on('end', function () {
+          done();
+        });
     });
 
     it('should generate a package.json file', function () {
       assert.file('temp/package.json');
+    });
+
+    it('should have populated package.json correctly', function (done) {
+      fs.readFile('temp/package.json', function (err, data) {
+        if (err) {
+          done(err);
+        } else {
+          var packageJson = JSON.parse(data);
+          assert.equal(packageJson.name, 'meantestappname');
+          assert.equal(packageJson.description, 'Testing the generator');
+          assert.equal(packageJson.author, 'Test');
+          done();
+        }
+      });
     });
   });
 
