a1b35b789d04906c10a709f7809b1d755e57492f
senpairamen <erfani.ramin@gmail.com>
2015-10-06 10:45:33 +0200

    Changed index.js to use slugified version of this.name where needed.

--- express-module/index.js
+++ express-module/index.js
@@ -10,42 +10,42 @@ module.exports = generators.NamedBase.extend({
     },
 
     createFolders: function() {
-        mkdirp("modules/" + this.name);
-        mkdirp("modules/" + this.name + "/server");
-        mkdirp("modules/" + this.name + "/server/controllers");
-        mkdirp("modules/" + this.name + "/server/models");
-        mkdirp("modules/" + this.name + "/server/policies");
-        mkdirp("modules/" + this.name + "/server/routes");
-        mkdirp("modules/" + this.name + "/tests");
+        mkdirp("modules/" + this.moduleName);
+        mkdirp("modules/" + this.moduleName + "/server");
+        mkdirp("modules/" + this.moduleName + "/server/controllers");
+        mkdirp("modules/" + this.moduleName + "/server/models");
+        mkdirp("modules/" + this.moduleName + "/server/policies");
+        mkdirp("modules/" + this.moduleName + "/server/routes");
+        mkdirp("modules/" + this.moduleName + "/tests");
     },
 
     createFiles: function() {
         this.fs.copyTpl(
             this.templatePath("module.server.controller.js"),
-            this.destinationPath("modules/" + this.name  + "/server/controllers/" + this.name + ".server.controller.js"),
+            this.destinationPath("modules/" + this.moduleName  + "/server/controllers/" + this.moduleName + ".server.controller.js"),
             {
-                moduleName: this.moduleName,
+                moduleName: this.name,
                 capitalizedModuleName: this.capitalizedModuleName
             });
         this.fs.copyTpl(
             this.templatePath("module.server.model.js"),
-            this.destinationPath("modules/" + this.name  + "/server/models/" + this.name + ".server.model.js"),
+            this.destinationPath("modules/" + this.moduleName  + "/server/models/" + this.moduleName + ".server.model.js"),
             {
-                moduleName: this.moduleName,
+                moduleName: this.name,
                 capitalizedModuleName: this.capitalizedModuleName
             });
         this.fs.copyTpl(
             this.templatePath("module.server.policy.js"),
-            this.destinationPath("modules/" + this.name  + "/server/policies/" + this.name + ".server.policy.js"),
+            this.destinationPath("modules/" + this.moduleName  + "/server/policies/" + this.moduleName + ".server.policy.js"),
             {
-                moduleName: this.moduleName,
+                moduleName: this.name,
                 capitalizedModuleName: this.capitalizedModuleName
             });
         this.fs.copyTpl(
             this.templatePath("module.server.routes.js"),
-            this.destinationPath("modules/" + this.name  + "/server/routes/" + this.name + ".server.routes.js"),
+            this.destinationPath("modules/" + this.moduleName  + "/server/routes/" + this.moduleName + ".server.routes.js"),
             {
-                moduleName: this.moduleName,
+                moduleName: this.name,
                 capitalizedModuleName: this.capitalizedModuleName
             });
     }
