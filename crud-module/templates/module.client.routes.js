63e7847487ebf441dabad9ca32194a2d286b4fb2
This Is His <his@Thiss-MacBook-Pro.local>
2015-11-20 16:10:05 -0600

    initial commit

---
+++ crud-module/templates/module.client.routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Setting up route
+angular.module('<%= moduleName %>').config(['$stateProvider',
+    function ($stateProvider) {
+
+        // <%= capitalizedModuleName %> state routing
+
+    }
+]);
