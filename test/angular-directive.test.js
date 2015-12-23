9139a48d14ebbcf4b5f4cf39a4066ca1a70bae06
Keith Levi Lumanog <keith@webninjamobile.com>
2015-12-23 18:28:57 +0800

    angular and directive mocha test

---
+++ test/angular-directive.test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var path = require('path'),
+  fs = require('fs-extra'),
+  helpers = require('yeoman-generator').test,
+  assert = require('yeoman-generator').assert,
+  tempDir,
+  temp = require('temp').track();
+
+describe('Angular Directive Subgenerator', function () {
+  this.timeout(0);
+  /**
+   * Setup the temp directory
+   */
+  before(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp/modules/core'), done);
+  });
+
+  /**
+   * Clean up temp directory
+   */
+  after(function () {
+    fs.removeSync(tempDir);
+    fs.removeSync(path.join(__dirname, 'temp'));
+  });
+
+  describe('Generate an Angular Directive file', function () {
+    beforeEach(function (done) {
+      helpers.run(path.join(__dirname, '../angular-directive'))
+        .inTmpDir(function (dir) {
+          tempDir = dir;
+          fs.copySync(path.join(__dirname, 'temp'), dir)
+        })
+        .withOptions({
+          'skip-install': true
+        })
+        .withArguments(['foo'])
+        .withPrompts({
+          'moduleName': 'core'
+        })
+        .on('ready', function (generator) {
+          // this is called right before `generator.run()` is called
+        })
+        .on('end', function () {
+          done();
+        });
+    });
+
+    it('should generate an angular directive file', function () {
+      assert.file(tempDir+'/modules/core/client/directives/foo.client.controller.js');
+    });
+
+
+  });
+});
