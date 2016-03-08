e8c78e8dee2505ee1b6ffdcb8032bb6474ca51be
Taylor Sarrafian <taylorsarrafian@gmail.com>
2016-03-08 13:58:55 +0000

    app tests using options

--- test/app.test.js
+++ test/app.test.js
@@ -25,7 +25,7 @@ describe('Main Generator', function () {
 
     rimraf(path.join(__dirname, 'temp'), done);
   });
-
+    
   describe('Application generator without sample module 0.4.0', function () {
     before(function (done) {
       helpers.run(path.join(__dirname, '../app'))
@@ -42,6 +42,65 @@ describe('Main Generator', function () {
           appAuthor: 'Test',
           addArticleExample: false,
           addChatExample: false
+        })
+        .on('ready', function (generator) {
+          // this is called right before `generator.run()` is called
+        })
+        .on('end', function () {
+          done();
+        });
+    });
+
+    it('should generate a package.json file', function () {
+      assert.file('temp/package.json');
+    });
+
+    it('should have populated package.json correctly', function (done) {
+      fs.readFile('temp/package.json', function (err, data) {
+        if (err) {
+          done(err);
+        } else {
+          var packageJson = JSON.parse(data);
+          assert.equal(packageJson.name, 'meantestappname');
+          assert.equal(packageJson.description, 'Testing the generator');
+          assert.equal(packageJson.author, 'Test');
+          done();
+        }
+      });
+    });
+    
+    it('should generate a bower.json file', function () {
+      assert.file('temp/bower.json');
+    });
+    
+    it('should populate the bower.json correctly', function (done) {
+      fs.readFile('temp/bower.json', function (err, data) {
+        if(err) {
+          done(err);
+        } else {
+          var bowerJson = JSON.parse(data);
+          assert.equal(bowerJson.name, 'meantestappname');
+          assert.equal(bowerJson.description, 'Testing the generator');
+          done();
+        }
+      });
+    });
+    
+  });
+    
+  describe('Application generator without sample module 0.4.0 using options', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../app'))
+        .withOptions({
+          'skip-install': true,
+          'version': '0.4.0',
+          'folder': 'temp',
+          'appName': 'MEANtestAppName',
+          'appDescription': 'Testing the generator',
+          'appKeywords': 'MongoDB, Express, AngularJS, Node.js',
+          'appAuthor': 'Test',
+          'addArticleExample': false,
+          'addChatExample': false
         })
         .on('ready', function (generator) {
           // this is called right before `generator.run()` is called
@@ -150,6 +209,66 @@ describe('Main Generator', function () {
     
   });
 
+
+  describe('Application generator without sample module 0.4.1 using options', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../app'))
+        .withOptions({
+          'skip-install': true,
+          'version': '0.4.1',
+          'folder': 'temp',
+          'appName': 'MEANtestAppName041',
+          'appDescription': 'Testing the generator 041',
+          'appKeywords': 'MongoDB, Express, AngularJS, Node.js',
+          'appAuthor': 'Test',
+          'addArticleExample': false,
+          'addChatExample': false
+        })
+        .on('ready', function (generator) {
+          // this is called right before `generator.run()` is called
+        })
+        .on('end', function () {
+          done();
+        });
+    });
+
+    it('should generate a package.json file', function () {
+      assert.file('temp/package.json');
+    });
+
+    it('should have populated package.json correctly', function (done) {
+      fs.readFile('temp/package.json', function (err, data) {
+        if (err) {
+          done(err);
+        } else {
+          var packageJson = JSON.parse(data);
+          assert.equal(packageJson.name, 'meantestappname041');
+          assert.equal(packageJson.description, 'Testing the generator 041');
+          assert.equal(packageJson.author, 'Test');
+          done();
+        }
+      });
+    });
+    
+    it('should generate a bower.json file', function () {
+      assert.file('temp/bower.json');
+    });
+    
+    it('should populate the bower.json correctly', function (done) {
+      fs.readFile('temp/bower.json', function (err, data) {
+        if(err) {
+          done(err);
+        } else {
+          var bowerJson = JSON.parse(data);
+          assert.equal(bowerJson.name, 'meantestappname041');
+          assert.equal(bowerJson.description, 'Testing the generator 041');
+          done();
+        }
+      });
+    });
+    
+  });
+
   describe('Application generator without sample module 0.4.2', function () {
     before(function (done) {
       helpers.run(path.join(__dirname, '../app'))
@@ -166,6 +285,69 @@ describe('Main Generator', function () {
           appAuthor: 'Test',
           addArticleExample: false,
           addChatExample: false
+        })
+        .on('ready', function (generator) {
+          // this is called right before `generator.run()` is called
+        })
+        .on('end', function () {
+          done();
+        });
+    });
+
+    it('should generate a package.json file', function () {
+      assert.file('temp/package.json');
+    });
+
+    it('should have populated package.json correctly', function (done) {
+      fs.readFile('temp/package.json', function (err, data) {
+        if (err) {
+          done(err);
+        } else {
+          var packageJson = JSON.parse(data);
+          assert.equal(packageJson.name, 'meantestappname042');
+          assert.equal(packageJson.description, 'Testing the generator 042');
+          assert.equal(packageJson.author, 'Test');
+          
+          // Testing the meanjs version was introduced in 0.4.2
+          assert.equal(packageJson['meanjs-version'], '0.4.2');
+          
+          done();
+        }
+      });
+    });
+    
+    it('should generate a bower.json file', function () {
+      assert.file('temp/bower.json');
+    });
+    
+    it('should populate the bower.json correctly', function (done) {
+      fs.readFile('temp/bower.json', function (err, data) {
+        if(err) {
+          done(err);
+        } else {
+          var bowerJson = JSON.parse(data);
+          assert.equal(bowerJson.name, 'meantestappname042');
+          assert.equal(bowerJson.description, 'Testing the generator 042');
+          done();
+        }
+      });
+    });
+    
+  });
+
+  describe('Application generator without sample module 0.4.2 using options', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../app'))
+        .withOptions({
+          'skip-install': true,
+	  'version': '0.4.2',
+	  'folder': 'temp',
+	  'appName': 'MEANtestAppName042',
+	  'appDescription': 'Testing the generator 042',
+	  'appKeywords': 'MongoDB, Express, AngularJS, Node.js',
+	  'appAuthor': 'Test',
+	  'addArticleExample': false,
+	  'addChatExample': false
         })
         .on('ready', function (generator) {
           // this is called right before `generator.run()` is called
