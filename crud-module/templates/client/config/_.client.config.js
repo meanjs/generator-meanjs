8c632dc5a9437db107c368d38b6167013e13f726
Rodrigo Muniz <muniz95@users.noreply.github.com>
2016-09-29 17:23:17 -0300

    CRUD modules #250
    Correction of this issue: https://github.com/meanjs/generator-meanjs/issues/250

--- crud-module/templates/client/config/_.client.config.js
+++ crud-module/templates/client/config/_.client.config.js
@@ -5,11 +5,11 @@
     .module('<%= slugifiedPluralName %>')
     .run(menuConfig);
 
-  menuConfig.$inject = ['menuService'];
+  menuConfig.$inject = ['Menus'];
 
-  function menuConfig(menuService) {
+  function menuConfig(Menus) {
     // Set top bar menu items
-    menuService.addMenuItem('topbar', {
+    Menus.addMenuItem('topbar', {
       title: '<%= humanizedPluralName %>',
       state: '<%= slugifiedPluralName %>',
       type: 'dropdown',
@@ -17,13 +17,13 @@
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', '<%= slugifiedPluralName %>', {
+    Menus.addSubMenuItem('topbar', '<%= slugifiedPluralName %>', {
       title: 'List <%= humanizedPluralName %>',
       state: '<%= slugifiedPluralName %>.list'
     });
 
     // Add the dropdown create item
-    menuService.addSubMenuItem('topbar', '<%= slugifiedPluralName %>', {
+    Menus.addSubMenuItem('topbar', '<%= slugifiedPluralName %>', {
       title: 'Create <%= humanizedSingularName %>',
       state: '<%= slugifiedPluralName %>.create',
       roles: ['user']
